dcdab39402cb9fb5dc7272107d9d1b9b3da1bff8
baharkholdisabeti <43182069+baharkholdisabeti@users.noreply.github.com>
2019-02-25 20:58:41 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -5,14 +5,14 @@ var x = setInterval(function() {
 // recieving response (time left)
 chrome.runtime.sendMessage({method:"getTimerMessage"},function(response){
   //here response will be the word you want
-  document.getElementById("basicUsage").innerHTML = response;
+  document.getElementById("countdown").innerHTML = response;
 });
 
 }, 1000);   
 
 // options button
 let options = document.getElementById('options');
-options.innerHTML = "Options";
-options.onclick = function(element) {	
-  chrome.tabs.create({'url': "/options.html" });
-};
+//options.innerHTML="options";
+options.onclick = function(element) {
+	chrome.tabs.create({ 'url': 'chrome://extensions/?options=' + chrome.runtime.id });
+}
